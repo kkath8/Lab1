3c48032d953ad464fecd82c6eb15c3eed1a77afe
kkath8 <alvarezkath08@gmail.com>
2025-09-24 22:02:06 +0800

    Add files via upload

---
+++ Operators.php
@@ -0,0 +1,27 @@
+<?php
+$a = 10;
+$b = 5;
+
+// Arithmetic Operators
+echo "<h3>Arithmetic Operators</h3>";
+echo "\$a + \$b = " . ($a + $b) . "<br>";
+echo "\$a - \$b = " . ($a - $b) . "<br>";
+echo "\$a * \$b = " . ($a * $b) . "<br>";
+echo "\$a / \$b = " . ($a / $b) . "<br>";
+echo "\$a % \$b = " . ($a % $b) . "<br>";
+
+// Comparison Operators
+echo "<h3>Comparison Operators</h3>";
+echo "\$a == \$b: " . ($a == $b ? "true" : "false") . "<br>";
+echo "\$a != \$b: " . ($a != $b ? "true" : "false") . "<br>";
+echo "\$a > \$b: " . ($a > $b ? "true" : "false") . "<br>";
+echo "\$a < \$b: " . ($a < $b ? "true" : "false") . "<br>";
+echo "\$a >= \$b: " . ($a >= $b ? "true" : "false") . "<br>";
+echo "\$a <= \$b: " . ($a <= $b ? "true" : "false") . "<br>";
+
+// Logical Operators
+echo "<h3>Logical Operators</h3>";
+echo "(\$a > 0 && \$b > 0): " . (($a > 0 && $b > 0) ? "true" : "false") . "<br>";
+echo "(\$a > 0 || \$b < 0): " . (($a > 0 || $b < 0) ? "true" : "false") . "<br>";
+echo "!(\$a == \$b): " . (!($a == $b) ? "true" : "false") . "<br>";
+?>
